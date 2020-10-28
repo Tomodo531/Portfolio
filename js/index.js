753728349624371d1169af12c85862ceed36c615
Martin Hesselbæk Olesen <47330347+Tomodo531@users.noreply.github.com>
2020-10-28 14:43:55 +0100

    update

--- js/index.js
+++ js/index.js
@@ -42,6 +42,25 @@ if (isMobile) {
 	});
 }
 
+var checkbox = document.querySelector('input[name=mode]');
+
+checkbox.addEventListener('change', function() {
+	if (this.checked) {
+		trans();
+		document.documentElement.setAttribute('data-theme', 'dark');
+	} else {
+		trans();
+		document.documentElement.setAttribute('data-theme', 'light');
+	}
+});
+
+let trans = () => {
+	document.documentElement.classList.add('transition');
+	window.setTimeout(() => {
+		document.documentElement.classList.remove('transition');
+	}, 1000);
+};
+
 var keyArray = [];
 var checkArray = [
 	'ArrowUp',
